52504b04289c7f218b843cecbfad1c2c45c1e054
Dudi <duygusu29@abv.bg>
2026-07-07 17:43:24 +0300

    Added basic properties and a constructor to the class  Room then created the methods ToFileRow and PrintRoomInfo

--- Room.cs
+++ Room.cs
@@ -8,5 +8,38 @@ namespace HotelProject
 {
     internal class Room
     {
+        public string roomNumber {  get; set; }
+        public string type { get; set; }
+        public int capacity { get; set; }
+        public double pricePerNight { get; set; }
+        public bool occupied { get; set; }
+        public string guestName { get; set; }
+
+       public Room(string roomNumber, string type, int capacity, double pricePerNight, bool occupied, string guestName)
+        {
+            this.roomNumber = roomNumber;
+            this.type = type;
+            this.capacity = capacity;
+            this.pricePerNight = pricePerNight;
+            this.occupied = occupied;
+            this.guestName = guestName;
+        }
+
+        public string ToFileRow()
+        {
+            return $"{roomNumber},{type},{capacity},{pricePerNight},{occupied},{guestName}"; 
+        }
+
+        public void PrintRoomInfo()
+        {
+           if (occupied == true)
+            {
+                Console.WriteLine("Стая: " + roomNumber + " | Тип: " +  type + " | Капацитет: " + capacity + " души | Цена: " + pricePerNight + " евро | Статус: Заета от " + guestName );
+            }
+           else
+            {
+                Console.WriteLine("Стая: " + roomNumber + " | Тип: " + type + " | Капацитет: " + capacity + " души | Цена: " + pricePerNight + " евро | Статус: Свободна");
+            }
+        }
     }
 }
